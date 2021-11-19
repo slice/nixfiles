7fd7410cc0049d23583cbbb644b51fc030a83e34
slice <tinyslices@gmail.com>
2021-11-19 04:08:48 -0800

    neovim: add nice comments

--- home/neovim/default.nix
+++ home/neovim/default.nix
@@ -43,62 +43,62 @@ in {
     extraConfig = lua (builtins.readFile ./init.lua);
 
     plugins = with pkgs.vimPlugins; [
-      vim-dirvish           # a better built-in directory browser
-      vim-sneak             # sneak around
-      vim-easy-align        # text alignment
-      vim-rsi               # readline keybindings in insert and command mode
-      vim-scriptease        # utilities for vim scripts
-      vim-eunuch            # unix shell commands, but in vim
-      vim-commentary        # comment stuff out
-      vim-unimpaired        # handy mappings
-      vim-surround          # easily edit surrounding characters
-      vim-fugitive          # a git wrapper so good, it should be illegal
-      vim-rhubarb           # github support for fugitive
-      vim-repeat            # . more stuff
-      vim-abolish           # better abbrevs, variant searching, and other stuff
+      vim-dirvish    # a better built-in directory browser
+      vim-sneak      # sneak around
+      vim-easy-align # text alignment
+      vim-rsi        # readline keybindings in insert and command mode
+      vim-scriptease # utilities for vim scripts
+      vim-eunuch     # unix shell commands, but in vim
+      vim-commentary # comment stuff out
+      vim-unimpaired # handy mappings
+      vim-surround   # easily edit surrounding characters
+      vim-fugitive   # a git wrapper so good, it should be illegal
+      vim-rhubarb    # github support for fugitive
+      vim-repeat     # . more stuff
+      vim-abolish    # better abbrevs, variant searching, and other stuff
 
-      vim-sayonara          # close buffers more intuitively
-      {
-        plugin = indent-blankline-nvim; # indentation guides
+      vim-sayonara   # close buffers more intuitively
+      {              # indentation guides
+        plugin = indent-blankline-nvim;
         config = lua ''
           require("indent_blankline").setup {
             buftype_exclude = {"terminal", "help"}
           }
         '';
       }
-      vim-rooter            # cd to the project root
+      vim-rooter     # cd to the project root
 
-      (plug {
+      (plug {        # open file manager or terminal
         url = "https://github.com/justinmk/vim-gtfo";
         rev = "7c7a495210a82b93e39bda72c6e4c59642dc4b07";
         sha256 = "sha256-XmYMlXk5xY/RFhnefCKbXUL9KeIXxY3QF3CFfKvEnus=";
       })
-      (plug {
+      (plug {        # superpowers for ctrl-x and ctrl-a
         url = "https://github.com/Konfekt/vim-CtrlXA";
         rev = "404ea1e055921db5679b3734108d72850d6faa76";
         sha256 = "sha256-ryf/nPbtfQL/RW+zha0O0kKQzArXHkG7Hp5ixi32b4E";
       })
 
-      # colorschemes
-      seoul256-vim
-      jellybeans-vim
-      zenburn
-      (plug {
+                     # colorschemes
+      seoul256-vim   # a nice pastel color scheme
+      jellybeans-vim # it really does look like jellybeans
+      zenburn        # alien salad
+      (plug {        # nice colors on gray
         url = "https://github.com/baskerville/bubblegum";
         rev = "92a5069edec4de88c31a4f1fdbcff34535951f8b";
         sha256 = "sha256-RSd+/kn4A6HA4vQHHlYTBxwccCeIiqmyaSKezXZc81c";
       })
-      (plug {
+      (plug {        # pitch black + neon
         url = "https://github.com/bluz71/vim-moonfly-colors";
         rev = "759cc4490e317bc641e4cd94b2c264d35b797d05";
         sha256 = "sha256-NLxBPUlXYRX5GpJT/8qmYCm81C75mXLojRwKzod1X2M=";
       })
-      (plug {
+      (plug {        # it's like being underwater!
         url = "https://github.com/bluz71/vim-nightfly-guicolors";
         rev = "6b73be294090e5793f8df0e60742d380f32bb1f2";
         sha256 = "sha256-bDeS+UQNxCZEOaWpNHxE7kRzHYNB9y+o2AXvmILe13A=";
       })
-      (plug {
+      (plug {        # pitch black + retro colors?
         url = "https://github.com/itchyny/landscape.vim";
         rev = "dcdd360f98d35d3b2c0bd1ce9f29ee053f906d07";
         sha256 = "sha256-rzr9lHFoAIyYWHebZhgvHH09dx/Wey4tudQBB+GjY6A";
