098a634f61fc01009979d95263ee9390dddee582
slice <tinyslices@gmail.com>
2021-11-18 23:32:24 -0800

    neovim: add important attribution

--- home/neovim.nix
+++ home/neovim.nix
@@ -1,6 +1,29 @@
 { pkgs, ... }:
 
 let
+  # function stolen from: https://github.com/nbardiuk/dotfiles/blob/b82ca0d28ba3726c46350b3d1063c4259dced2d9/nix/.config/nixpkgs/home/nvim.nix
+  #
+  # Copyright © 2020 Nazarii Bardiuk
+  # 
+  # Permission is hereby granted, free of charge, to any person obtaining a copy of
+  # this software and associated documentation files (the “Software”), to deal in
+  # the Software without restriction, including without limitation the rights to
+  # use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+  # of the Software, and to permit persons to whom the Software is furnished to do
+  # so, subject to the following conditions:
+  #
+  # The above copyright notice and this permission notice shall be included in all
+  # copies or substantial portions of the Software.
+  #
+  # THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  # AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+  # SOFTWARE.
+  #
+  # ... thanks!
   plug = { url, rev, sha256 }:
     pkgs.vimUtils.buildVimPluginFrom2Nix {
       pname = with pkgs.lib; last (splitString "/" url);
