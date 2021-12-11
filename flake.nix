891abb7dd2e1e0719815651dfb8b114c44ee630f
slice <tinyslices@gmail.com>
2021-12-10 17:49:29 -0800

    add x86_64 home config to flake

--- flake.nix
+++ flake.nix
@@ -20,21 +20,34 @@
     };
   };
 
-  outputs = { self, darwin, nixpkgs, home-manager, fenix, ... }@inputs: {
-    packages = {
-      aarch64-darwin.homeConfigurations.slice =
-        home-manager.lib.homeManagerConfiguration {
-          system = "aarch64-darwin";
-          username = "slice";
-          homeDirectory = "/Users/slice";
-          configuration.imports = [ ./home/home.nix ];
-          extraModules = [ ./modules/hh3.nix ];
-          extraSpecialArgs = { server = false; };
-          # needed to avoid a conflict; see https://github.com/nix-community/home-manager/issues/2073
-          stateVersion = "21.05";
-        };
-    };
+  outputs = { self, darwin, nixpkgs, home-manager, fenix, ... }@inputs:
+    (let
+      hmConfig = {
+        configuration.imports = [ ./home/home.nix ];
+        extraModules = [ ./modules/hh3.nix ];
+      };
+    in {
+      packages = {
+        x86_64-linux.homeConfigurations.slice =
+          home-manager.lib.homeManagerConfiguration (hmConfig // {
+            system = "x86_64-linux";
+            username = "slice";
+            homeDirectory = "/home/slice";
+            extraSpecialArgs = { server = true; };
+            # needed to avoid a conflict; see https://github.com/nix-community/home-manager/issues/2073
+            stateVersion = "21.05";
+          });
+        aarch64-darwin.homeConfigurations.slice =
+          home-manager.lib.homeManagerConfiguration (hmConfig // {
+            system = "aarch64-darwin";
+            username = "slice";
+            homeDirectory = "/Users/slice";
+            extraSpecialArgs = { server = false; };
+            # ditto
+            stateVersion = "21.05";
+          });
+      };
 
-    darwinConfigurations.dewey = (import ./hosts/dewey.nix) inputs;
-  };
+      darwinConfigurations.dewey = (import ./hosts/dewey.nix) inputs;
+    });
 }
