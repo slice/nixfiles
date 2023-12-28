4640606cea5b3468a787da7b0ce38f993bc833de
slice <tinyslices@gmail.com>
2023-12-28 14:20:00 -0800

    Thu Dec 28 14:20:00 PST 2023

--- flake.nix
+++ flake.nix
@@ -67,7 +67,7 @@
             specialArgs = {
               server = false;
               # build FFmpeg with libfdk-aac support
-              customFFmpeg = true;
+              customFFmpeg = false;
 
               # create out of store symlinks for neovim's configuration files
               # for faster editing
