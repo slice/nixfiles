d67ab4f53a4c797ede313d63637680b75a9069f5
Skip R <tinyslices@gmail.com>
2024-06-22 14:13:06 -0700

    update

--- flake.nix
+++ flake.nix
@@ -7,7 +7,7 @@
     flake-utils.url = "github:numtide/flake-utils";
 
     lix = {
-      url = "git+https://git@git.lix.systems/lix-project/lix?ref=refs/tags/2.90-beta.1";
+      url = "git+https://git@git.lix.systems/lix-project/lix?ref=refs/tags/2.90.0-rc1";
       flake = false;
     };
     lix-module = {
