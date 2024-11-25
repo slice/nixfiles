9e5cb2a00093ef49f7f5aab71886317f2868d070
Skip R <tinyslices@gmail.com>
2024-11-25 02:55:18 -0800

    nvim: nice fugitive binds

--- nvim/lua/skip/plugins/fugitive.lua
+++ nvim/lua/skip/plugins/fugitive.lua
@@ -4,9 +4,12 @@ return {
     cmd = 'Git',
     lazy = false,
     keys = {
-      { '<Leader>a', '<Cmd>vert G<CR>', desc = 'Git' },
-      { '<Leader>q', '<Cmd>.GBrowse!<CR>', desc = '.GBrowse!' },
+      { '<Leader>a',  '<Cmd>vert G<CR>',       desc = 'Git' },
+      { '<Leader>q',  '<Cmd>.GBrowse!<CR>',    desc = '.GBrowse!' },
       { '<Leader>jd', '<Cmd>Git difftool<CR>', desc = 'Git difftool' },
+      { '<Leader>jv', '<Cmd>Gvdiffsplit<CR>',  desc = 'Gvdiffsplit' },
+      { '<Leader>js', '<Cmd>Gdiffsplit<CR>',   desc = 'Gdiffsplit' },
+      { '<Leader>jc', ':G commit -m "',        desc = ':G commit -m "' },
     },
   },
 }
