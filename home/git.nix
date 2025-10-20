89f46fe11e9a8922bef430fefeeb5253946fc1ee
Skip R <tinyslices@gmail.com>
2025-10-19 19:22:56 -0700

    update git config

--- home/git.nix
+++ home/git.nix
@@ -1,38 +1,31 @@
 { pkgs, ... }:
 
 {
+  programs.delta = {
+    enable = true;
+    enableGitIntegration = true;
+    options = {
+      line-numbers = true;
+      features = "decorations";
+      hyperlinks = true;
+      syntax-theme = "zenburn";
+      side-by-side = false;
+    };
+  };
+
   programs.git = {
     enable = true;
-    userName = "Skip R";
-    userEmail = "tinyslices@gmail.com";
 
-    lfs.enable = true;
-
-    delta = {
-      enable = true;
-      options = {
-        line-numbers = true;
-        features = "decorations";
-        hyperlinks = true;
-        syntax-theme = "zenburn";
-        side-by-side = false;
-      };
-    };
-
-    includes = [
-      {
-        path = "~/.config/git/work";
-        condition = "gitdir:~/work/";
-      }
-    ];
-
-    extraConfig =
+    settings =
       let
         ghCredentialHelper = {
           helper = "!${pkgs.gh}/bin/gh auth git-credential";
         };
       in
       {
+        user.name = "Skip R";
+        user.email = "tinyslices@gmail.com";
+
         # url."git@github.com:".insteadOf = "https://github.com/";
         commit.verbose = true;
         format.pretty = "tformat:%C(bold yellow)%h%Creset %<|(82,trunc)%s %Creset%C(bold white)%cr%C(nobold)/%ch%Creset %C(bold)(%an)%C(auto)%d";
@@ -50,6 +43,15 @@
         "credential \"https://github.com\"" = ghCredentialHelper;
         "credential \"https://gist.github.com\"" = ghCredentialHelper;
       };
+
+    lfs.enable = true;
+
+    includes = [
+      {
+        path = "~/.config/git/work";
+        condition = "gitdir:~/work/";
+      }
+    ];
 
     ignores = [
       "*~"
