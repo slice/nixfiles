8ab47a074e4571eeccb04b11f311b57616349b1d
Skip R <tinyslices@gmail.com>
2026-04-05 18:39:51 -0700

    nvim/lsp: revive semantic tokens

--- nvim/lua/skip/lsp.lua
+++ nvim/lua/skip/lsp.lua
@@ -250,9 +250,4 @@ try_adding_capabilities(function()
   return require('lsp-file-operations').default_capabilities()
 end)
 
--- don't advertise semantic tokens support, crow doesn't support em just yet.
--- applied last so it can't be overwritten by capability sources above
-M.capabilities.textDocument = M.capabilities.textDocument or {}
-M.capabilities.textDocument.semanticTokens = vim.NIL
-
 return M
